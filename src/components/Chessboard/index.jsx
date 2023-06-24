d784b85db2d2077ef4c96ec5d4142b565f91edcc
Francogomez23800 <francogomez23800@gmail.com>
2023-06-24 20:35:37 -0300

    Movement control

--- src/components/Chessboard/index.jsx
+++ src/components/Chessboard/index.jsx
@@ -1,51 +1,54 @@
 import React, { useRef } from "react";
 import { Tile } from "../Tile";
 import "./chessboard.css";
+import { useState } from "react";
 
 const verticalAxis = [1, 2, 3, 4, 5, 6, 7, 8];
 const horizontalAxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
-const pieces = [{ image: String, x: Number, y: Number }];
+const initialBoardState = [{ image: String, x: Number, y: Number }];
 
 //Pawns
 for (let i = 0; i < 8; i++) {
-  pieces.push({ image: "assets/images/pawn_b.png", x: i, y: 6 });
+  initialBoardState.push({ image: "assets/images/pawn_b.png", x: i, y: 6 });
 }
 for (let i = 0; i < 8; i++) {
-  pieces.push({ image: "assets/images/pawn_w.png", x: i, y: 1 });
+  initialBoardState.push({ image: "assets/images/pawn_w.png", x: i, y: 1 });
 }
 
 //Rocks
-pieces.push({ image: "assets/images/rook_b.png", x: 0, y: 7 });
-pieces.push({ image: "assets/images/rook_b.png", x: 7, y: 7 });
+initialBoardState.push({ image: "assets/images/rook_b.png", x: 0, y: 7 });
+initialBoardState.push({ image: "assets/images/rook_b.png", x: 7, y: 7 });
 
-pieces.push({ image: "assets/images/rook_w.png", x: 0, y: 0 });
-pieces.push({ image: "assets/images/rook_w.png", x: 7, y: 0 });
+initialBoardState.push({ image: "assets/images/rook_w.png", x: 0, y: 0 });
+initialBoardState.push({ image: "assets/images/rook_w.png", x: 7, y: 0 });
 
 //Knights
-pieces.push({ image: "assets/images/knight_b.png", x: 1, y: 7 });
-pieces.push({ image: "assets/images/knight_b.png", x: 6, y: 7 });
+initialBoardState.push({ image: "assets/images/knight_b.png", x: 1, y: 7 });
+initialBoardState.push({ image: "assets/images/knight_b.png", x: 6, y: 7 });
 
-pieces.push({ image: "assets/images/knight_w.png", x: 1, y: 0 });
-pieces.push({ image: "assets/images/knight_w.png", x: 6, y: 0 });
+initialBoardState.push({ image: "assets/images/knight_w.png", x: 1, y: 0 });
+initialBoardState.push({ image: "assets/images/knight_w.png", x: 6, y: 0 });
 
 //Bishops
-pieces.push({ image: "assets/images/bishop_b.png", x: 2, y: 7 });
-pieces.push({ image: "assets/images/bishop_b.png", x: 5, y: 7 });
+initialBoardState.push({ image: "assets/images/bishop_b.png", x: 2, y: 7 });
+initialBoardState.push({ image: "assets/images/bishop_b.png", x: 5, y: 7 });
 
-pieces.push({ image: "assets/images/bishop_w.png", x: 2, y: 0 });
-pieces.push({ image: "assets/images/bishop_w.png", x: 5, y: 0 });
+initialBoardState.push({ image: "assets/images/bishop_w.png", x: 2, y: 0 });
+initialBoardState.push({ image: "assets/images/bishop_w.png", x: 5, y: 0 });
 
 //Queens
-pieces.push({ image: "assets/images/queen_b.png", x: 3, y: 7 });
-pieces.push({ image: "assets/images/queen_w.png", x: 4, y: 0 });
+initialBoardState.push({ image: "assets/images/queen_b.png", x: 3, y: 7 });
+initialBoardState.push({ image: "assets/images/queen_w.png", x: 4, y: 0 });
 
 //Kings
-pieces.push({ image: "assets/images/king_b.png", x: 4, y: 7 });
-pieces.push({ image: "assets/images/king_w.png", x: 3, y: 0 });
+initialBoardState.push({ image: "assets/images/king_b.png", x: 4, y: 7 });
+initialBoardState.push({ image: "assets/images/king_w.png", x: 3, y: 0 });
 
 const Chessboard = () => {
+  const [pieces, setPieces] = useState(initialBoardState)
   const chessboardRef =useRef(null)
   let activePiece = null;
+
 
   function grabPiece(e) {
     const element = e.target;
@@ -61,35 +64,35 @@ const Chessboard = () => {
     }
   }
   
-  function movePiece(e) {
-    const chessboard = chessboardRef.current;
-  
-    if (activePiece && chessboard) {
-      const minX = chessboard.offsetLeft - 25;
-      const minY = chessboard.offsetTop - 25;
-      const maxX = chessboard.offsetLeft + chessboard.offsetWidth - 75;
-      const maxY = chessboard.offsetTop + chessboard.offsetHeight - 75;
-      const x = e.clientX - 50;
-      const y = e.clientY - 50;
-      activePiece.style.position = 'absolute';
-  
-      if (x < minX) {
-        activePiece.style.left = `${minX}px`;
-      } else if (x > maxX) {
-        activePiece.style.left = `${maxX}px`;
-      } else {
-        activePiece.style.left = `${x}px`;
-      }
-  
-      if (y < minY) {
-        activePiece.style.top = `${minY}px`;
-      } else if (y > maxY) {
-        activePiece.style.top = `${maxY}px`;
-      } else {
-        activePiece.style.top = `${y}px`;
-      }
+function movePiece(e) {
+  const chessboard = chessboardRef.current;
+
+  if (activePiece && chessboard) {
+    const minX = chessboard.offsetLeft - 25;
+    const minY = chessboard.offsetTop - 25;
+    const maxX = chessboard.offsetLeft + chessboard.offsetWidth - 75;
+    const maxY = chessboard.offsetTop + chessboard.offsetHeight - 75;
+    const x = e.clientX - 50;
+    const y = e.clientY - 50;
+    activePiece.style.position = 'absolute';
+
+    if (x < minX) {
+      activePiece.style.left = `${minX}px`;
+    } else if (x > maxX) {
+      activePiece.style.left = `${maxX}px`;
+    } else {
+      activePiece.style.left = `${x}px`;
+    }
+
+    if (y < minY) {
+      activePiece.style.top = `${minY}px`;
+    } else if (y > maxY) {
+      activePiece.style.top = `${maxY}px`;
+    } else {
+      activePiece.style.top = `${y}px`;
     }
   }
+}
   
   function dropPiece(e) {
     if (activePiece) {
@@ -97,7 +100,6 @@ const Chessboard = () => {
     }
   }
   
-
   let board = [];
 
   for (let j = verticalAxis.length - 1; j >= 0; j--) {
